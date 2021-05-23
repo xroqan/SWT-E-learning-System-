fcefaf3824c8521007f44e5b2a86fda8b2ce36cf
User <User@Vusal>
2021-05-23 20:14:51 +0200

    Änderung

--- SWT/src/Main.java
+++ SWT/src/Main.java
@@ -7,10 +7,11 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JButton;
+import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class Dateiordner extends JFrame {
+public class Main extends JFrame {
 
 	private JPanel contentPane;
 
@@ -21,7 +22,7 @@ public class Dateiordner extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Dateiordner frame = new Dateiordner();
+					Main frame = new Main();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -33,44 +34,46 @@ public class Dateiordner extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public Dateiordner() {
+	public Main() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(0, 102, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Dateiornder");
+		JLabel lblNewLabel = new JLabel("Hauptmen\u00FC");
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 35));
-		lblNewLabel.setBounds(38, 10, 272, 29);
+		lblNewLabel.setBounds(104, 0, 301, 51);
 		contentPane.add(lblNewLabel);
 		
-		JButton btnNewButton = new JButton("X");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Main obj = new Main();
-				obj.setVisible(true);
-			}
-		});
-		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 20));
-		btnNewButton.setBounds(351, 0, 85, 21);
-		contentPane.add(btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("Hochladen");
-		btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 11));
-		btnNewButton_1.setBounds(10, 217, 118, 36);
+		JButton btnNewButton_1 = new JButton("Arbeitsraum");
+		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		btnNewButton_1.setBounds(57, 61, 301, 36);
 		contentPane.add(btnNewButton_1);
 		
-		JButton btnNewButton_1_1 = new JButton("Herunterladen");
-		btnNewButton_1_1.setFont(new Font("Tahoma", Font.BOLD, 11));
-		btnNewButton_1_1.setBounds(148, 217, 118, 36);
+		JButton btnNewButton_1_1 = new JButton("Dateiordner\r\n");
+		btnNewButton_1_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Dateiordner obj = new Dateiordner();
+				obj.setVisible(true);
+				
+			}
+		});
+		btnNewButton_1_1.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		btnNewButton_1_1.setBounds(57, 114, 301, 36);
 		contentPane.add(btnNewButton_1_1);
 		
-		JButton btnNewButton_1_2 = new JButton("L\u00F6schen");
-		btnNewButton_1_2.setFont(new Font("Tahoma", Font.BOLD, 11));
-		btnNewButton_1_2.setBounds(284, 217, 118, 36);
+		JButton btnNewButton_1_2 = new JButton("Forum\r\n");
+		btnNewButton_1_2.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		btnNewButton_1_2.setBounds(57, 160, 301, 36);
 		contentPane.add(btnNewButton_1_2);
+		
+		JButton btnNewButton_1_3 = new JButton("Gruppenverwaltung\r\n");
+		btnNewButton_1_3.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		btnNewButton_1_3.setBounds(57, 206, 301, 36);
+		contentPane.add(btnNewButton_1_3);
 	}
 
 }
